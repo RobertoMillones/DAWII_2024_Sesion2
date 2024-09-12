7769cd12dd9aefbf5e9dbb2bf8604d858cc4634c
RobertoMillones <roberto.millonesperez26@gmail.com>
2024-09-11 22:11:07 -0500

    el primer commit

---
+++ src/test/java/com/prestamo/DawiSesion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiSesion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
